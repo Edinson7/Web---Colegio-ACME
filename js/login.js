7891b4e131b8e733875e12185fc5324b3dd03e59
carloshuila <carloshuila@unicauca.edu.co>
2020-06-12 00:40:36 -0500

    login-validar

--- js/login.js
+++ js/login.js
@@ -1,28 +1,44 @@
 const btnLogin = document.getElementById('btnLogin');
+const txtUser = document.getElementById('txtUser');
+const txtPass = document.getElementById('txtPass');
 
-function asignarVista (pTipo) {
-  switch (pTipo) {
-    case "administrador":
-      location.href = "admin-inicio.html";
-      break;
-    case "profesor":
-      location.href = "profesor-inicio.html";
-      break;
-    case "estudiante":
-      location.href = "estudiante-inicio.html";
-      break;
-  }
+function asignarVista(pTipo) {
+   switch (pTipo) {
+      case "administrador":
+         location.href = "admin-inicio.html";
+         break;
+      case "profesor":
+         location.href = "profesor-inicio.html";
+         break;
+      case "estudiante":
+         location.href = "estudiante-inicio.html";
+         break;
+   }
 }
 
 btnLogin.addEventListener('click', function() {
-  let txtUser = document.getElementById('txtUser').value;
-  let txtPass = document.getElementById('txtPass').value;
+   const usuarioValue = txtUser.value.trim();
+   const passwordValue = txtPass.value.trim();
 
-  for(let i=0; i < cuentas.length; i++) {
-    if(cuentas[i].user === txtUser && cuentas[i].password === txtPass) {
-      asignarVista(cuentas[i].tipo);
-    }
-  }
+   if (usuarioValue === '') {
+      setErrorFor(txtUser, 'No se puede dejar el usuario en blanco.');
+   } else {
+      setSuccessFor(txtUser);
+   }
+   if (passwordValue === '') {
+      setErrorFor(txtPass, 'No se puede dejar la contraseña en blanco.');
+   } else {
+      setSuccessFor(txtPass);
+   }
+
+   for (let i = 0; i < cuentas.length; i++) {
+      if (cuentas[i].user === usuarioValue && cuentas[i].password === passwordValue) {
+         asignarVista(cuentas[i].tipo);
+      }
+      else {
+         console.log("usuario o contraseña incorrecta");
+      }
+   }
 });
 
 
