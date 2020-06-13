12e68b5a15186174adfdbc766e58b7f3520221e7
carloshuila <carloshuila@unicauca.edu.co>
2020-06-13 10:08:29 -0500

    validar-login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 const btnLogin = document.getElementById('btnLogin');
 const txtUser = document.getElementById('txtUser');
 const txtPass = document.getElementById('txtPass');
+var errorMensaje = document.getElementById("field-error");
+
 
 function asignarVista(pTipo) {
    switch (pTipo) {
@@ -19,27 +21,35 @@ function asignarVista(pTipo) {
 btnLogin.addEventListener('click', function() {
    const usuarioValue = txtUser.value.trim();
    const passwordValue = txtPass.value.trim();
+   let userAcceso = false;
+   let passAcceso = false;
 
    if (usuarioValue === '') {
       setErrorFor(txtUser, 'No se puede dejar el usuario en blanco.');
    } else {
       setSuccessFor(txtUser);
+      userAcceso = true;
    }
    if (passwordValue === '') {
       setErrorFor(txtPass, 'No se puede dejar la contraseña en blanco.');
    } else {
       setSuccessFor(txtPass);
+      passAcceso = true;
+   }
+   if (userAcceso == true && passAcceso === true) {
+      for (let i = 0; i < cuentas.length; i++) {
+         if (cuentas[i].user === usuarioValue && cuentas[i].password === passwordValue) {
+            asignarVista(cuentas[i].tipo);
+         } else {
+            errorMensaje.style.display = 'block';
+            errorMensaje.innerHTML = 'Usuario o contraseña incorrecta'
+         }
+      }
    }
 
-   for (let i = 0; i < cuentas.length; i++) {
-      if (cuentas[i].user === usuarioValue && cuentas[i].password === passwordValue) {
-         asignarVista(cuentas[i].tipo);
-      }
-      else {
-         console.log("usuario o contraseña incorrecta");
-      }
-   }
+
 });
+
 
 function setSuccessFor(input) {
    const formControl = input.parentElement;
